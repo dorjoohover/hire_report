8c83c16598b4fbc9175b5e27c9b24210061ec2fe
dorjoo <dorjoohover@gmail.com>
2025-12-29 18:42:17 +0800

    env config fix

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import { setupSwagger } from './config/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
-  app.listen(4000, '::', () => {
-    console.log('Server running on port 4000');
-  });
+  const port = process.env.REPORT_PORT || 4000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
