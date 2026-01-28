5b23bf1f6e27cba308c22b974c204e9057025ccb
dorjoo <dorjoohover@gmail.com>
2026-01-29 06:00:09 +0800

    a

--- src/file.service.ts
+++ src/file.service.ts
@@ -130,6 +130,7 @@ export class FileService {
 private async downloadFromS3(k: string): Promise<Buffer | null> {
   try {
     let key = k.replace('report-', '').replace('.pdf', '')
+    console.log('key ',key, ' k ', k )
     const possibleKeys = [
       key.endsWith('.pdf') ? key : `${key}.pdf`,
       `report-${key}.pdf`,
