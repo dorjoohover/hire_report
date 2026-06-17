4cd099651d20f76d37ea65d2de2ce15806087840
Dorjoo <dorjoohover@gmail.com>
2026-06-17 08:33:54 +0800

    changed aws env name"

--- src/file.service.ts
+++ src/file.service.ts
@@ -32,8 +32,8 @@ export class FileService {
 
   constructor() {
     this.s3 = new AWS.S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_KEY,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       region: process.env.AWS_REGION,
       httpOptions: {
         timeout: 600000,
